f04928f8dbf803751a720ddf75bdb371094d55ea
nosike22 <frank.nosike@gmail.com>
2022-02-14 16:00:57 +0100

    This is my status

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            account account1 = new account("Nosike Franklin", 500.00m);
+            account account2 = new account("Naomi Nosike",300.00m);
+            account account3 = new account("Sophia Nosike",100.00m);
+
+            Console.WriteLine($"{account1.Name} Has a balance : {account1.balance}");
+            Console.WriteLine($"{account2.Name} Has a balance : {account2.balance}");
+            Console.WriteLine($"{account3.Name} Has a balance : {account3.balance}");
+
+            Console.WriteLine("\nEnter the amount you want to deposit into account1:");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount: C}to account1 balance/n");
+            account1.Deposit(depositAmount);
+
+            Console.WriteLine($"{account1.Name} Has a balance : {account1.balance}");
+            Console.WriteLine($"{account2.Name} Has a balance : {account2.balance}");
+            Console.WriteLine($"{account3.Name} Has a balance : {account3.balance}");
+
+            Console.WriteLine("\nEnter the amount you want to deposit into account2:");
+             depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount: C}to account2 balance/n");
+            account2.Deposit(depositAmount);
+
+
+            Console.WriteLine($"{account1.Name} Has a balance : {account1.balance}");
+            Console.WriteLine($"{account2.Name} Has a balance : {account2.balance}");
+            Console.WriteLine($"{account3.Name} Has a balance : {account3.balance}");
+
+            Console.WriteLine("\nEnter the amount you want to deposit into account3:");
+            depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount: C}to account3 balance/n");
+            account3.Deposit(depositAmount);
+
+            Console.WriteLine($"{account1.Name} Has a balance : {account1.balance}");
+            Console.WriteLine($"{account2.Name} Has a balance : {account2.balance}");
+            Console.WriteLine($"{account3.Name} Has a balance : {account3.balance}");
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    class account
+    {
+        public string Name{ get; set; }
+        public decimal balance;
+
+        public account(string accountName, decimal initialBalance)
+        {
+            Name = accountName;
+            Balance = initialBalance;
+        }
+
+        private decimal Balance
+        {
+            get
+            {
+                return Balance;
+
+            }
+
+            set
+            {
+                if(value  >= 0.0m)
+                {
+                    Balance = value;
+                }
+            }
+        }
+        public void Deposit(decimal depositAmount)
+        {
+            if (depositAmount > balance)
+            {
+                Balance = Balance + depositAmount;
+
+            }
+        }
+    }
+}
